db43c71c6d728d2b3c6eae831c0d6572eeaa28c4
iysheng <iysheng@163.com>
2016-11-04 17:38:33 +0800

    Update motor.c
    添加了读取对应方向的距离数据

--- motor.c
+++ motor.c
@@ -17,11 +17,14 @@
 #include <linux/uaccess.h>
 #include <linux/device.h>
 #include <linux/hrtimer.h>
+#include <linux/delay.h>
 
 #define NAME_LEN 16
 
 /*gpio5_8*/
 //#define MOTOR_GPIO_NUM 168
+
+#define RADAR_TIME_SCALE 1000
 
 /*gpio5_9*/
 #define LED_GPIO_NUM 169
@@ -34,6 +37,10 @@
 static int motor_major = MOTOR_MAJOR;
 static dev_t motor_dev_num;
 int motor_gpio[MOTOR_NUM] = {113};
+int trig_gpio[MOTOR_NUM] = {110};
+int echo_gpio[MOTOR_NUM] = {111};
+
+
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("yang yongsheng<iysheng@163.com>");
@@ -41,14 +48,24 @@ MODULE_VERSION("0.1");
 
 struct motor_dev{
 	struct cdev cdev;
-	int num;
-	unsigned int pwm_value_us;
+	int num;//舵机引脚
+	int trig_num;//雷达触发引脚
+	int echo_num;//雷达接受引脚
+	unsigned int radar_time;//雷达时间
+	unsigned int pwm_value_us;//舵机高脉冲时间
+	unsigned int echo_irq_num;
+	struct hrtimer radar_hrt;
 	struct hrtimer hrt;
+	struct semaphore radar_sem;
 	char name[NAME_LEN];
 	bool pwm_state;
+	bool hrt_state;
+	bool radar_hrt_state;
 } *motor_devp;
 
 struct class *motor_class;
+
+
 
 static enum hrtimer_restart motor_hrtimer_callback(struct hrtimer *hrt)
 {
@@ -60,8 +77,39 @@ static enum hrtimer_restart motor_hrtimer_callback(struct hrtimer *hrt)
 	hrtimer_forward_now(hrt, ns_to_ktime((20000-devp_tmp->pwm_value_us)*1000));//T=20,000ns
 	else if(devp_tmp->pwm_state == true)
 	hrtimer_forward_now(hrt, ns_to_ktime((devp_tmp->pwm_value_us) * 1000));//T=20,000ns
-	//printk(KERN_INFO "hrt time is %d",devp_tmp->pwm_value_us);
 	return HRTIMER_RESTART;
+}
+
+static enum hrtimer_restart radar_hrtimer_callback(struct hrtimer *hrt)
+{
+	struct motor_dev *devp_tmp;
+	devp_tmp = container_of(hrt, struct motor_dev, radar_hrt);
+	(devp_tmp->radar_time)++;
+	hrtimer_forward_now(hrt, ns_to_ktime(RADAR_TIME_SCALE));
+	return HRTIMER_RESTART;
+}
+
+irqreturn_t radar_echo_interrupt(int irq, void *dev_id)
+{
+	struct motor_dev *devp_tmp = (struct motor_dev *)dev_id;
+	if (gpio_get_value(devp_tmp->echo_num))//上升沿触发		
+	{
+		if(!down_trylock(&devp_tmp->radar_sem))
+		{
+		hrtimer_init(&devp_tmp->radar_hrt, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+		devp_tmp->radar_hrt.function = radar_hrtimer_callback;
+		devp_tmp->radar_time = 0;
+		hrtimer_start(&devp_tmp->radar_hrt, ns_to_ktime(RADAR_TIME_SCALE), HRTIMER_MODE_REL);
+		devp_tmp->radar_hrt_state = false;
+		}
+	}
+	else  if (!gpio_get_value(devp_tmp->echo_num))//下降沿触发
+	{	
+		devp_tmp->radar_hrt_state = true;
+		up(&devp_tmp->radar_sem);
+		hrtimer_cancel(&devp_tmp->radar_hrt);
+	}
+	return IRQ_HANDLED;
 }
 
 int motor_open (struct inode *inode, struct file *filp)
@@ -69,10 +117,16 @@ int motor_open (struct inode *inode, struct file *filp)
 	struct motor_dev *devp_tmp;
 	devp_tmp = container_of(inode->i_cdev, struct motor_dev, cdev);
 	filp->private_data = devp_tmp;
-	hrtimer_init(&devp_tmp->hrt, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
-	devp_tmp->hrt.function = motor_hrtimer_callback;
-	gpio_set_value(devp_tmp->num, devp_tmp->pwm_state);
-	hrtimer_start(&devp_tmp->hrt, ns_to_ktime(devp_tmp->pwm_value_us), HRTIMER_MODE_REL);
+	if(devp_tmp->hrt_state == false)
+	{
+		hrtimer_init(&devp_tmp->hrt, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+		devp_tmp->hrt.function = motor_hrtimer_callback;
+		gpio_set_value(devp_tmp->num, devp_tmp->pwm_state);
+		hrtimer_start(&devp_tmp->hrt, ns_to_ktime(devp_tmp->pwm_value_us), HRTIMER_MODE_REL);	
+		devp_tmp->hrt_state = true;
+		devp_tmp->radar_hrt_state = false;
+	}
+	sema_init(&devp_tmp->radar_sem, 1);
 	printk(KERN_INFO "motor_open.\n");
 	return 0;
 }
@@ -101,22 +155,40 @@ ssize_t motor_write (struct file *filp, const char __user *buf, size_t count, lo
 	return count;
 }
 
+ssize_t radar_read (struct file *filp, char __user *buf, size_t count, loff_t *ppos)
+{
+	struct motor_dev *motor_devp_tmp = filp->private_data;
+	gpio_set_value(motor_devp_tmp->trig_num, false);
+	udelay(10);
+	gpio_set_value(motor_devp_tmp->trig_num, true);
+	udelay(100);
+	gpio_set_value(motor_devp_tmp->trig_num, false);
+	mdelay(10);
+	down(&motor_devp_tmp->radar_sem);
+	if(motor_devp_tmp->radar_hrt_state == true)
+		printk(KERN_INFO "radar_read ente radar_timer is %dus and radar_len is %dmm.\n", motor_devp_tmp->radar_time, 34*(motor_devp_tmp->radar_time)/100);
+	up(&motor_devp_tmp->radar_sem);
+	return 0;
+}
+
+
 static struct file_operations motor_fops = 
 {
 	.owner = THIS_MODULE,
 	.open = motor_open,
 	.release = motor_release,
 	.write = motor_write,
+	.read = radar_read,
 };
 
-int motor_setup(struct motor_dev *devp, int min, int num)
+int motor_setup(struct motor_dev *devp, int min, int num, int trig, int echo)
 {
 	int ret = 0; 
 	sprintf(devp->name, "motor%d", min);	
 	if(!gpio_is_valid(num))
 	{
 		printk(KERN_INFO "invalid motor%d_gpio_num:%d.\n",min, num);
-		return -ENODEV;
+		goto fail_num;
 	}
 	else
 	{
@@ -124,9 +196,37 @@ int motor_setup(struct motor_dev *devp, int min, int num)
 	gpio_direction_output(num, false);
 	gpio_export(num, false);	/*cause GPIO_NUM to appear in /sys/class/gpio*/
 	}
+	if(!gpio_is_valid(trig))
+	{
+		printk(KERN_INFO "invalid motor%d_gpio_trig:%d.\n",min, trig);
+		goto fail_trig;
+	}
+	else
+	{
+	gpio_request(trig,"sysfs");
+	gpio_direction_output(trig, false);
+	gpio_export(trig, false);
+	}
+	if(!gpio_is_valid(echo))
+	{
+		printk(KERN_INFO "invalid motor%d_gpio_trig:%d.\n",min, trig);
+		goto fail_echo;
+	}
+	else
+	{
+	gpio_request(echo,"sysfs");
+	gpio_direction_input(echo);
+	gpio_export(echo, false);
+	}
 	devp->num = num;
+	devp->trig_num = trig;
+	devp->echo_num = echo;
 	devp->pwm_value_us = 1500;//500~2500 us
 	devp->pwm_state = true;
+	devp->hrt_state = false;
+	devp->radar_hrt_state = false;
+	devp->echo_irq_num = gpio_to_irq(echo);
+	request_irq(devp->echo_irq_num, radar_echo_interrupt, IRQF_TRIGGER_RISING|IRQF_TRIGGER_FALLING, "radar_echo_handler", devp);
 	devp->cdev.owner = THIS_MODULE;
 	cdev_init(&devp->cdev, &motor_fops);
 	ret = cdev_add(&devp->cdev, MKDEV(motor_major, min), 1);
@@ -139,6 +239,12 @@ int motor_setup(struct motor_dev *devp, int min, int num)
 		device_create(motor_class, NULL, MKDEV(motor_major, min), NULL,devp->name);
 	}
 	return 0; 
+fail_echo:
+	gpio_unexport(trig);
+fail_trig:
+	gpio_unexport(num);
+fail_num:
+	return -ENODEV;
 }
 
 int __init motor_init(void)
@@ -177,10 +283,9 @@ int __init motor_init(void)
 	printk(KERN_INFO "motor_major is %d\n", motor_major);
 	for(i=0; i<MOTOR_NUM; i++)
 	{
-		motor_setup(motor_devp+i, i, motor_gpio[i]);
+		motor_setup(motor_devp+i, i, motor_gpio[i], trig_gpio[i], echo_gpio[i]);
 	}
-
-	
+		
 	gpio_request(LED_GPIO_NUM, "sysfs");
 	gpio_direction_output(LED_GPIO_NUM, true);
 	gpio_export(LED_GPIO_NUM, false);/*cause LED_GPIO_NUM to appear in /sys/class/gpio*/
@@ -199,11 +304,18 @@ void __exit motor_exit(void)
 		hrtimer_cancel(&(motor_devp+i)->hrt);
 		gpio_unexport((motor_devp+i)->num);
 		gpio_free((motor_devp+i)->num);
+		gpio_unexport((motor_devp+i)->trig_num);
+		gpio_free((motor_devp+i)->trig_num);
+		gpio_unexport((motor_devp+i)->echo_num);
+		gpio_free((motor_devp+i)->echo_num);
+		free_irq((motor_devp+i)->echo_irq_num, (motor_devp+i));
 		device_destroy(motor_class,MKDEV(motor_major,i));
 		cdev_del(&(motor_devp+i)->cdev);
 		unregister_chrdev_region(MKDEV(motor_major, i), 1);
 		kfree(motor_devp+i);
 	}
+
+	
 	gpio_unexport(LED_GPIO_NUM);
 	gpio_free(LED_GPIO_NUM);
 	class_destroy(motor_class);	
